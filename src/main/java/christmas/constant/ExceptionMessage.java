74e1de77ac9b1d67a5a5c1371b59db7938f42a95
Jung Yunho <a01049048063@gmail.com>
2023-11-11 02:22:57 +0900

    feat(ReservationDate): 날짜 범위가 아니면 예외처리

--- src/main/java/christmas/constant/ExceptionMessage.java
+++ src/main/java/christmas/constant/ExceptionMessage.java
@@ -2,7 +2,8 @@ package christmas.constant;
 
 public enum ExceptionMessage {
     NOT_INITIALIZED("도메인 객체가 초기화되지 않았습니다."),
-    NOT_NUMBER("입력값은 숫자여야 합니다.");
+    NOT_NUMBER("입력값은 숫자여야 합니다."),
+    INVALID_DATE("날짜는 1 ~ 31 사이의 범위여야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
 
--- src/main/java/christmas/domain/ReservationDate.java
+++ src/main/java/christmas/domain/ReservationDate.java
@@ -1,10 +1,22 @@
 package christmas.domain;
 
+import christmas.constant.ExceptionMessage;
+
 public class ReservationDate {
 
-    private final Number value;
+    private static final int START_DATE = 1;
+    private static final int END_DATE = 31;
 
-    public ReservationDate(final Integer value) {
-        this.value = new Number(value);
+    private final Number number;
+
+    public ReservationDate(final Integer number) {
+        validateDate(number);
+        this.number = new Number(number);
+    }
+
+    private void validateDate(final Integer value) {
+        if (value < START_DATE || value > END_DATE) {
+            throw new IllegalArgumentException(ExceptionMessage.NOT_INITIALIZED.toMessage());
+        }
     }
 }
