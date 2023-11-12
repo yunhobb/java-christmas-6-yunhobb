f4c537ad36c6fafb7dd930d851cba46145ef7ea4
Jung Yunho <a01049048063@gmail.com>
2023-11-12 18:10:14 +0900

    fix(ReservatioinDate): 예약날짜 맴버변수 LocalDate로 변경

--- src/main/java/christmas/domain/ReservationDate.java
+++ src/main/java/christmas/domain/ReservationDate.java
@@ -1,21 +1,24 @@
 package christmas.domain;
 
 import christmas.constant.ExceptionMessage;
+import java.time.LocalDate;
 
 public class ReservationDate {
 
     private static final int START_DATE = 1;
     private static final int END_DATE = 31;
+    private static final int EVENT_YEAR = 2023;
+    private static final int EVNET_MONTH = 12;
 
-    private final Number number;
+    private final LocalDate localDate;
 
-    public ReservationDate(final Integer number) {
-        validateDate(number);
-        this.number = new Number(number);
+    public ReservationDate(final Integer date) {
+        validateDate(date);
+        this.localDate = LocalDate.of(EVENT_YEAR, EVNET_MONTH, date);
     }
 
-    private void validateDate(final Integer value) {
-        if (value < START_DATE || value > END_DATE) {
+    private void validateDate(final Integer date) {
+        if (date < START_DATE || date > END_DATE) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_INITIALIZED.toMessage());
         }
     }
