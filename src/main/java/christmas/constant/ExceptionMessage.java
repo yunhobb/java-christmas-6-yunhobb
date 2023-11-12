b5abe1904a0d5288f22d6b4ede14f9d153c9da74
Jung Yunho <a01049048063@gmail.com>
2023-11-12 15:31:51 +0900

    feat(OrderMenu): 메뉴가 없거나 1개 미만으로 주문시 에외처리

--- src/main/java/christmas/constant/ExceptionMessage.java
+++ src/main/java/christmas/constant/ExceptionMessage.java
@@ -4,7 +4,10 @@ public enum ExceptionMessage {
     NOT_INITIALIZED("도메인 객체가 초기화되지 않았습니다."),
     NOT_NUMBER("입력값은 숫자여야 합니다."),
     INVALID_DATE("날짜는 1 ~ 31 사이의 범위여야 합니다."),
-    INVALID_ORDER_MENU_FORMAT("잘못된 주문 형식입니다.");
+    INVALID_ORDER_MENU_FORMAT("잘못된 주문 형식입니다."),
+    INVALID_ORDER_MENU("없는 메뉴입니다."),
+    INVALID_ORDER_COUNT("1개 이상을 주문 하셔야 합니다.");
+
 
     private static final String ERROR_TAG = "[ERROR] ";
 
--- src/main/java/christmas/domain/OrderMenu.java
+++ src/main/java/christmas/domain/OrderMenu.java
@@ -1,11 +1,16 @@
 package christmas.domain;
 
+import christmas.constant.ChristmasMenu;
 import christmas.constant.ExceptionMessage;
 import christmas.constant.RegexPattern;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OrderMenu {
 
+    private static final String MENU_COUNT_DELIMITER = "-";
+    private static final int MIN_COUNT = 1;
     private final List<String> elements;
 
     public OrderMenu(List<String> elements) {
@@ -18,6 +23,30 @@ public class OrderMenu {
             if (RegexPattern.isNotOrderMenuFormat(element)) {
                 throw new IllegalArgumentException(ExceptionMessage.INVALID_ORDER_MENU_FORMAT.toMessage());
             }
+        }
+    }
+
+    public Map<String, Integer> getMenuWithCount() {
+        Map<String, Integer> menuWithCount = new HashMap<>();
+
+        for (String element : elements) {
+            String[] parts = element.split(MENU_COUNT_DELIMITER);
+            validateMenu(parts[0]);
+            validateMenuCount(parts[1]);
+            menuWithCount.put(parts[0], Integer.parseInt(parts[1]));
+        }
+        return menuWithCount;
+    }
+
+    private void validateMenu(String part) {
+        if (ChristmasMenu.isNotIncludeMenu(part)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_ORDER_MENU.toMessage());
+        }
+    }
+
+    private void validateMenuCount(String part) {
+        if (Integer.parseInt(part) < MIN_COUNT) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_ORDER_COUNT.toMessage());
         }
     }
 
