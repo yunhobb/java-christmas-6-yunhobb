a4721f6c4c96f8ccf4dd5b17d2f8ba050a714159
Jung Yunho <a01049048063@gmail.com>
2023-11-15 03:56:48 +0900

    test(InputValidatorTest): 입력값 숫자 검증 테스트

---
+++ src/test/java/christmas/io/InputValidatorTest.java
@@ -0,0 +1,39 @@
+package christmas.io;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import christmas.constant.RegexPattern;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("InputValidator의")
+class InputValidatorTest {
+
+    final InputValidator inputValidator = new InputValidator();
+
+
+    @DisplayName("숫자가 아니면 예외를 던지는")
+    @Test
+    void check_with_number() {
+        //given
+        final String input = "a";
+
+        //when & then
+        assertThatThrownBy(() -> inputValidator.validateNumeric(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자면 정상적으로 반환하는가")
+    @Test
+    void check_without_number() {
+        //given
+        final String input = "1";
+
+        //when & then
+        assertThatNoException()
+                .isThrownBy(() -> RegexPattern.isNotNumeric(input));
+    }
+
+
+}
